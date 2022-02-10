5c7771b9279940018e04743ff4a1f9c8881a0def
Gabriel Fonzare <gabrielfonzare@gmail.com>
2022-02-10 20:08:12 -0300

    first Commit

---
+++ Back/src/ProjetoAngularAPI/Controllers/EventoController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProjetoAngularAPI.Models;
+
+namespace ProjetoAngularAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[]{
+            new Evento(){
+            EventoId = 1,
+            Tema = "Curso Angular",
+            Local = "Araraquara",
+            Lote = "1º Lote",
+            QtdPessoas = 250,
+            DataEvento = DateTime.Now.AddDays(2).ToString(),
+            ImageURL = "foto.png"
+
+            },
+
+            new Evento(){
+            EventoId = 2,
+            Tema = "Curso Bootstrap",
+            Local = "Araraquara",
+            Lote = "2º Lote",
+            QtdPessoas = 250,
+            DataEvento = DateTime.Now.AddDays(10).ToString(),
+            ImageURL = "foto.png"
+
+            }
+             
+            };
+
+        public EventoController()
+        {
+           
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+           return _evento;
+        }
+
+         [HttpGet("{id}")]
+        public IEnumerable<Evento> Get(long id)
+        {
+           return _evento.Where(x=> x.EventoId == id);
+        }
+    }
+}
